b81e3730c96c2b37b6e7a2957c74f86fbf456736
student <student@STEP.LAN>
2017-03-11 12:14:26 +0200

    MD5

---
+++ Project1/Encoder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Project1
+{
+    class Encoder
+    {        
+        MD5 coder;
+
+        public Encoder()
+        {
+            coder = MD5.Create();
+        }
+
+        string GetMd5Hash(string message)
+        {
+            byte[] data = coder.ComputeHash(Encoding.UTF8.GetBytes(message));            
+            StringBuilder sBuilder = new StringBuilder();            
+            for (int i = 0; i < data.Length; i++)
+                sBuilder.Append(data[i].ToString("x2"));            
+            return sBuilder.ToString();
+        }
+
+    }
+}
